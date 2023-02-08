dbd1d15b53e4024d8aee8a31b2b4b4fe746709fe
David Gaspar <david.correa@intelbras.com.br>
2023-02-08 05:32:22 -0300

    chore: setup wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,23 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+#define WLAN_SSID ".Intelbras Coletores"
+#define WLAN_PASS "Intelbras@Coletores2018"
+
+void setupWifi() {
+  // Set credential to WiFi access point
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+  Serial.print("[+] Connecting on Wifi ");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\n[+] Wifi connected");
+}
 
 void setup() {
   // put your setup code here, to run once:
