c24b2b4412918794587f4ef24bf736a192f0d021
David Gaspar <david.correa@intelbras.com.br>
2023-02-08 13:25:02 -0300

    chore: add mqtt connection to publish

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 #define WLAN_SSID ".Intelbras Coletores"
 #define WLAN_PASS "Intelbras@Coletores2018"
@@ -19,10 +20,56 @@ void setupWifi() {
   Serial.println("\n[+] Wifi connected");
 }
 
-void setup() {
-  // put your setup code here, to run once:
+IPAddress server(10, 1, 15, 59);
+uint16_t port = 32354;
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+const char* clientId = "esp32";
+const char* user = "admin";
+const char* pass = "admin";
+
+void setupMqttClient() {
+  Serial.println("[+] (MQTT) Connecting");
+
+  client.setServer(server, port);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void reconnectMqtt() {
+  if(!client.connected()) {
+    Serial.println("[+] (MQTT) Reconnecting");
+
+    if(client.connect(clientId, 0, MQTTQOS1, 0, 0)) {
+      Serial.println("[+] (MQTT) Reconnected");
+    } else {
+      delay(1000);
+      Serial.println("[+] (MQTT) Try to reconnect again");
+      reconnectMqtt();
+    }
+  }
 }
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(460800);
+
+  setupWifi();
+  setupMqttClient();
+}
+
+unsigned int count = 0;
+
+void loop() {
+  reconnectMqtt();
+
+  delay(1000);
+
+  char msg[50];
+
+  sprintf(msg, "Joven -> %d", count);
+
+  client.publish("ESP32", msg);
+  Serial.print("[+] Message sent: ");
+  Serial.println(msg);
+
+  count++;
+}  // put your main code here, to run repeatedly:
