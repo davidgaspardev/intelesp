1cd9bc6fe51532d3778fc3fedce6da558c8f6519
David Gaspar <david.correa@intelbras.com.br>
2023-04-28 16:48:58 -0300

    feat: use MqttClient class

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,61 @@
 #include <Arduino.h>
 #include <Wifi.h>
-#include <PubSubClient.h>
 #include <LedOnboard.h>
-#include <esp_system.h>
+#include <InputPin.h>
+#include <Time.h>
+#include <MqttClient.h>
 
 #define WLAN_SSID ".Intelbras Coletores"
 #define WLAN_PASS "Intelbras@Coletores2018"
 
 LedOnboard ledOnboard;
 Wifi wifi;
-
-IPAddress server(10, 1, 15, 59);
-uint16_t port = 32354;
-PubSubClient client(wifi.client);
-const char* clientId = "esp32";
-
-void reconnectMqtt() {
-  if(!client.connected()) {
-    Serial.println("[+] (MQTT) Reconnecting");
-
-    if(client.connect(clientId, 0, MQTTQOS1, 0, 0)) {
-      Serial.println("[+] (MQTT) Reconnected");
-    } else {
-      delay(1000);
-      Serial.println("[+] (MQTT) Try to reconnect again");
-      reconnectMqtt();
-    }
-  }
-}
-
-void setupMqttClient() {
-  Serial.println("[+] (MQTT) Connecting");
-
-  client.setServer(server, port);
-  reconnectMqtt();
-}
+MqttClient mqttClient;
+Time timeNow;
+InputPin pin(GPIO_NUM_13);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(460800);
 
-  pinMode(GPIO_NUM_13, INPUT_PULLDOWN);
-
+  // Setuping Wifi
   wifi.connect(WLAN_SSID, WLAN_PASS);
-  setupMqttClient();
+
+  // Setuping MQTT Client
+  IPAddress ipAddr(10, 1, 15, 59);
+  uint16_t port = 32354;
+  mqttClient.setup(ipAddr, port, &wifi.client);
+
+  // Setuping time now
+  timeNow.setup();
 }
 
-unsigned int count = 0;
-uint8_t lastSignal = LOW;
-char msg[100];
-
 void loop() {
-  // Serial.print("[+] Message sent: ");
-  uint8_t signal = digitalRead(GPIO_NUM_13);
-  if (signal == HIGH && lastSignal == LOW) {
-    String mac = wifi.macAddress();
-    // Serial.println(mac.c_str());
-    sprintf(msg, "{\"pin\":13,\"mac\":\"%s\"}", mac.c_str());
-    if(!client.publish("ESP32", msg)) {
-      reconnectMqtt();
-    };
-    Serial.print("[+] Message sent: ");
-    Serial.println(msg);
-    count++;
-    lastSignal = signal;
-    return;
-  } else {
-    lastSignal = signal;
-  }
+  char msg[90]; // {"pin":13,"mac":"C8:F0:9E:51:64:F0","signal":"HIGH","date":"2023-04-28T20:12:41Z"}
 
-  if (signal == HIGH) {
-    ledOnboard.turnOn();
-  } else {
-    ledOnboard.turnOff();
+  if (pin.signalChanged()) {
+    char signal[5];
+    if(pin.getSignal() == HIGH) {
+      strcpy(signal, "HIGH");
+      ledOnboard.turnOn();
+    } else {
+      strcpy(signal, "LOW");
+      ledOnboard.turnOff();
+    }
+
+    String mac = wifi.macAddress();
+    sprintf(
+      msg,
+      "{\"pin\":13,\"mac\":\"%s\",\"signal\":\"%s\",\"date\":\"%sZ\"}",
+      mac.c_str(),
+      signal,
+      timeNow.getISOString()
+    );
+
+    if(!mqttClient.publish("ESP32", msg)) {
+      mqttClient.reconnect();
+    }
+    Serial.printf("[+] Message sent: %s\n", msg);
   }
 
   delay(100);
